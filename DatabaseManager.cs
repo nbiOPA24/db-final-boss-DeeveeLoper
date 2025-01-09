005816144326f86e60373c961d3e07e75f1f0fe8
RobCam <roberto.camacho4@gmail.com>
2025-01-09 16:21:44 +0100

    Initialize DatabaseManager with basic connection setup

---
+++ DatabaseManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+
+namespace SchoolPlatform;
+
+internal class DatabaseManager
+{
+    private IDbConnection Connect()
+    {
+        string connectionstring = File.ReadAllText("connectionstring.txt");
+        IDbConnection connection = new SqlConnection(connectionstring);
+        return connection;
+    }
+}
+
+
