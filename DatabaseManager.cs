f5caf9fd685300f648896bbe23195868bc19d99b
RobCam <roberto.camacho4@gmail.com>
2025-01-10 11:30:39 +0100

    Added methods: GetAllStudents, GetAllTeachers, and GetAllGuardians

--- DatabaseManager.cs
+++ DatabaseManager.cs
@@ -1,6 +1,6 @@
 ﻿using System.Data;
+using Dapper;
 using Microsoft.Data.SqlClient;
-
 
 namespace SchoolPlatform;
 
@@ -12,6 +12,26 @@ internal class DatabaseManager
         IDbConnection connection = new SqlConnection(connectionstring);
         return connection;
     }
-}
 
+    public IEnumerable<Student> GetAllStudents()
+    {
+        using IDbConnection connection = Connect();
+        IEnumerable<Student> students = connection.Query<Student>("SELECT * FROM Student");
+        return students;
+    }
+
+    public IEnumerable<Teacher> GetAllTeachers()
+    {
+        using IDbConnection connection = Connect();
+        IEnumerable<Teacher> teachers = connection.Query<Teacher>("SELECT * FROM Teacher");
+        return teachers;
+    }
+
+    public IEnumerable<Guardian> GetAllGuardians()
+    {
+        using IDbConnection connection = Connect();
+        IEnumerable<Guardian> teachers = connection.Query<Guardian>("SELECT * FROM Guardian");
+        return teachers;
+    }
+}
 
